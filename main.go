874ab55df1e87f52aa123e4a3573b112e9ef3f61
Sergey Protko <sergey.protko@yandex.ru>
2025-03-07 12:33:03 +0300

    add flags

--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/probeldev/go-multiline-formatter/formatter"
+	funcnameforlog "github.com/probeldev/go-multiline-formatter/funcNameForLog"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
+	multiline := flag.Bool("multiline", false, "Преоброазование одной строки в несколько")
+	logfunc := flag.Bool("logfunc", false, "Добавление строковой переменной с названием структуры и функции")
+
+	flag.Parse()
+
 	if scanner.Scan() {
 		input := scanner.Text()
-		f := formatter.GetFormatter()
-		output := f.OneLine2MultiLine(input)
-		fmt.Println(output)
+
+		if *multiline {
+			f := formatter.GetFormatter()
+			output := f.OneLine2MultiLine(input)
+			fmt.Println(output)
+		} else if *logfunc {
+			f := funcnameforlog.GetFuncNameForLog()
+			output := f.AddFuncName(input)
+			fmt.Println(output)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
